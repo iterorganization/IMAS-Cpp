93211ccb939cad8ad7fcb8477d11ed895eba5b30
Bartek Palak <bartek.palak@man.poznan.pl>
2021-12-10 10:59:08 +0100

    CPP: overloading blitz array assignment operator [IMAS-3937]

--- src/UALDef.h
+++ src/UALDef.h
@@ -28,7 +28,7 @@
 
 #include <blitz/memblock.h>
 #include <blitz/array.h>
-
+#include <stdio.h>
 
 typedef std::complex < double > std_complex_t;
 
@@ -44,13 +44,16 @@ typedef std::complex < double > std_complex_t;
  static const int   IDS_TIME_MODE_INDEPENDENT   = 2;
 
 
+// Overloading blitz::Array class to get knowledge about memory policy 
+// it is essential to have this knowledge to decide 
+// if data should be freed manually or automatically by blitz 
 template <typename P_numtype, int N_rank>
 class IMASArray : public blitz::Array<P_numtype, N_rank> 
 {
     using  blitz::Array<P_numtype, N_rank>::Array;
 
     private:
-        blitz::preexistingMemoryPolicy deletionPolicy = blitz::duplicateData;
+        blitz::preexistingMemoryPolicy deletionPolicy = blitz::deleteDataWhenDone;
 
     public:
         blitz::preexistingMemoryPolicy getDeletionPolicy(){
@@ -60,6 +63,21 @@ class IMASArray : public blitz::Array<P_numtype, N_rank>
         void setDeletionPolicy(blitz::preexistingMemoryPolicy deletionPolicy){
             this->deletionPolicy = deletionPolicy;
         }
+
+
+    typedef IMASArray<P_numtype, N_rank> T_array;
+
+
+    // Overloading assignment operator to set a proper memory policy
+    // (original operator copies everything including policy flag,
+    // that may be inproper for given array
+    T_array & operator=(T_array const &x )
+    {
+        T_array& returnArray =  (T_array &)blitz::Array<P_numtype, N_rank>::operator=(x);
+
+        returnArray.setDeletionPolicy(blitz::deleteDataWhenDone);
+        return returnArray;
+    }
 };
 
 
