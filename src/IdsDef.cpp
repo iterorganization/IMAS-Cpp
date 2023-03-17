b63e5d8e3dc8a9181c586a0e4f65bbc8fcf87700
Ludovic FLEURY <ludovic.fleury@cea.fr>
2023-03-17 15:21:33 +0100

    IMAS-4564: completing patch from commit 57b132b1f90

--- src/IdsDef.cpp
+++ src/IdsDef.cpp
@@ -1394,12 +1394,15 @@ bool IdsNs::Ids::isError(al_status_t al_status, const char *file, const unsigned
   		al_status = ual_read_data(ctx, fieldPath.c_str(), timeBasePath.c_str(), &ptrData, INTEGER_DATA, 1, &retSize[0]);
         if (al_status.code != 0)
     			return al_status;
-
+     
         if(ptrData == NULL || retSize[0] == 0)
+        {
+            array.free();
             return al_status;
+        }
 
-        free(ptrData);
-		IdsNs::Ids::setArray(array, (int*)ptrData, retSize[0]);
+
+	IdsNs::Ids::setArray(array, (int*)ptrData, retSize[0]);
 		
 
         return al_status;
@@ -1415,8 +1418,12 @@ bool IdsNs::Ids::isError(al_status_t al_status, const char *file, const unsigned
         if (al_status.code != 0)
     			return al_status;
 
-        if(ptrData == NULL || retSize[0] * retSize[1] == 0)
+
+       if(ptrData == NULL || retSize[0] * retSize[1] == 0)
+        {
+            array.free();
             return al_status;
+        }
 
 		IdsNs::Ids::setArray(array, (int*)ptrData, retSize[0], retSize[1]);
 		
@@ -1435,7 +1442,10 @@ bool IdsNs::Ids::isError(al_status_t al_status, const char *file, const unsigned
     			return al_status;
 
         if(ptrData == NULL || retSize[0] * retSize[1] * retSize[2] == 0)
+            {
+            array.free();
             return al_status;
+        }
 
 		IdsNs::Ids::setArray(array, (int*)ptrData, retSize[0], retSize[1], retSize[2]);
 		
@@ -1455,7 +1465,10 @@ bool IdsNs::Ids::isError(al_status_t al_status, const char *file, const unsigned
     			return al_status;
 
         if(ptrData == NULL || retSize[0] * retSize[1] * retSize[2] * retSize[3] == 0)
+            {
+            array.free();
             return al_status;
+        }
 
 		IdsNs::Ids::setArray(array, (int*)ptrData, retSize[0], retSize[1], retSize[2], retSize[3]);
 		
@@ -1474,7 +1487,10 @@ bool IdsNs::Ids::isError(al_status_t al_status, const char *file, const unsigned
     			return al_status;
 
         if(ptrData == NULL || retSize[0] * retSize[1] * retSize[2] * retSize[3] * retSize[4] * retSize[5] == 0)
+            {
+            array.free();
             return al_status;
+        }
 
 		IdsNs::Ids::setArray(array, (int*)ptrData, retSize[0], retSize[1], retSize[2], retSize[3], retSize[4], retSize[5]);
 		
@@ -1493,7 +1509,10 @@ bool IdsNs::Ids::isError(al_status_t al_status, const char *file, const unsigned
     			return al_status;
 
         if(ptrData == NULL || retSize[0] * retSize[1] * retSize[2] * retSize[3] * retSize[4] == 0)
+            {
+            array.free();
             return al_status;
+        }
 
 		IdsNs::Ids::setArray(array, (int*)ptrData, retSize[0], retSize[1], retSize[2], retSize[3], retSize[4]);
 		
