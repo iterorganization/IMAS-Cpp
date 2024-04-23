41c5b8caae247577b24a4339ac9c31791fe1213f
Bloch Pawel EXT <blochp@sdcc-login03.iter.org>
2024-04-23 09:12:02 +0200

    Cahnge char* to std::string [IMAS-5232]

--- examples/new_examples/example_001_open_database.cpp
+++ examples/new_examples/example_001_open_database.cpp
@@ -9,24 +9,18 @@ void create_db_entry_legacy()
 {
     // This example focuses on creating DBEntry using legacy mode method
 
-    char* userName      = getenv("USER");
-    char db_name[]      = "testdb";
-    int pulse           = 1;
-    int run             = 10;
-    char version[]      = "3";
-
-    if(userName == NULL) 
-    {
-        printf( "PANIC: $USER not found! Exiting...");
-        exit(1);
-    }
+    char* userName         = getenv("USER");
+    std::string db_name    = "testdb";
+    int pulse              = 1;
+    int run                = 10;
+    std::string dd_version = "3";
 
     // create data entry object (using legacy method, deprecated from AL5)
     IdsNs::IDS ids(pulse, run, 0, 0);
     ids.setBackend(HDF5_BACKEND);
     
     // this time we are creating completly new entry
-    ids.createEnv(userName, db_name, version);    
+    ids.createEnv(userName, db_name.c_str(), dd_version.c_str());    
     // to open existing entry use ids.openEnv(userName, db_name, version)
 
     // You can access IDSes in here - take a look at sample code dealing with IDSes for details
@@ -38,25 +32,19 @@ void open_db_entry_uri()
 {
     // This example focuses on opening DBEntry using URI
 
-    char* userName      = getenv("USER");
-    std::string db_name = "testdb";
-    int pulse           = 1;
-    int run             = 10;
-    char backend[]      = "hdf5";
-    std::string version = "3";
-
-    if(userName == NULL)
-    {
-        printf( "PANIC: $USER not found! Exiting...");
-        exit(1);
-    }
+    char* userName         = getenv("USER");
+    std::string db_name    = "testdb";
+    int pulse              = 1;
+    int run                = 10;
+    std::string backend    = "hdf5";
+    std::string dd_version = "3";
 
     // std::string uri = std::format("imas:{}?user={};pulse={};run={};database={};version={}",backend, userName, pulse, run, db_name, version);
 
     // If you're using a C++ version older than C++20, make sure to use string concatenation instead of std::format.
     
-    std::string uri = std::string("imas:")+std::string(backend) + std::string("?user=") + std::string(userName) + ";pulse=" + std::to_string(pulse)
-    + ";run=" + std::to_string(run) + ";database=" + std::string(db_name) + ";version=" + std::string(version);
+    std::string uri = std::string("imas:")+backend + std::string("?user=") + userName + ";pulse=" + std::to_string(pulse)
+    + ";run=" + std::to_string(run) + ";database=" + db_name + ";version=" + dd_version;
     
 
     IdsNs::IDS ids;
@@ -65,13 +53,7 @@ void open_db_entry_uri()
     //  OPEN_PULSE
     //  CREATE_PULSE
     //  FORCE_CREATE_PULSE
-
-    if (status != 0)
-    {
-        printf( "PANIC: Could not create pulsefile");
-        exit(1);
-    }
-
+    
     // You can access IDSes in here - take a look at sample code dealing with IDSes for details
 
     ids.close();
