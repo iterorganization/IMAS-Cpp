037656aa0813edf3dcdbc8cc5e6cd71918715fe4
Bloch Pawel EXT <blochp@sdcc-login03.iter.org>
2024-06-18 11:23:51 +0200

    [IMAS-5138] second version of the second tutorial

--- examples/new_examples/test_new_examples.cpp
+++ examples/new_examples/test_new_examples.cpp
@@ -1,4 +1,5 @@
 #include "example_001_open_database.cpp"
+#include "example_002_fill_data_in_ids.cpp.cpp"
 
 int main(int argc, char *argv[])
 {
@@ -6,7 +7,12 @@ int main(int argc, char *argv[])
     create_db_entry_legacy();
     open_db_entry_uri();
     create_db_entry_uri_with_path();
+
     //example002
+    creating_completly_new_ids();
+    default_values_and_aos_operations();
+    copying_and_validating_ids();
+
     //example003
     //example004
     return 0;
