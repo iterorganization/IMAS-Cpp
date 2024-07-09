5f4ee557ac697a0271a3592d8768e3661a0aa426
Bloch Pawel EXT <blochp@sdcc-login03.iter.org>
2024-07-09 08:23:08 +0200

    [IMAS-5150]  fix typo

--- doc/code_samples/example_004_read_data_from_entry.cpp
+++ doc/code_samples/example_004_read_data_from_entry.cpp
@@ -6,13 +6,12 @@ using namespace IdsNs;
 
 void read_entire_ids()
 {
-    
     // This example focuses on reading whole IDS from entry.
     // We are storing and reading back an IDS - equilibrium. Data are stored inside MDS+ file.
     
     // NOTE: this block of code uses 'FORCE_CREATE_PULSE' mode in order to create example data
     IdsNs::IDS ids;
-    int status = ids.open("imas:mdsplus?path=./testdb_mdsplus",FORCE_CREATE_PULSE);
+    int status = ids.open("imas:mdsplus?path=./testdb_mdsplus", FORCE_CREATE_PULSE);
 
     IdsNs:IDS::equilibrium  equilibrium;
 
@@ -33,7 +32,7 @@ void read_entire_ids()
     ids.close();
 
     // NOTE: this block of code uses 'OPEN_PULSE' mode in order to read example data
-    status = ids.open("imas:mdsplus?path=./testdb_mdsplus",OPEN_PULSE);
+    status = ids.open("imas:mdsplus?path=./testdb_mdsplus", OPEN_PULSE);
     ids._equilibrium.get();
     equilibrium = ids._equilibrium;
     
@@ -53,7 +52,7 @@ void read_slice()
 
     // NOTE: this block of code uses 'FORCE_CREATE_PULSE' mode in order to create example data
     IdsNs::IDS ids;
-    int status = ids.open("imas:mdsplus?path=./testdb_mdsplus",FORCE_CREATE_PULSE);
+    int status = ids.open("imas:mdsplus?path=./testdb_mdsplus", FORCE_CREATE_PULSE);
     
     // fill IDS with example data
     IdsNs::IDS::summary summary;
@@ -64,9 +63,9 @@ void read_slice()
         summary.global_quantities.ip.value(i) = 10+i;
     }
     summary.heating_current_drive.nbi.resize(1);
-    summary.heating_current_drive.nbi(0).beam_current_fraction.value.resize(3,100);
+    summary.heating_current_drive.nbi(0).beam_current_fraction.value.resize(3,3);
     for(int i=0;i<3;i++){
-        for(int j=0; j<100; j++){
+        for(int j=0; j<3; j++){
             summary.heating_current_drive.nbi(0).beam_current_fraction.value(i,j) = 100*i;
         }
     }
@@ -81,29 +80,29 @@ void read_slice()
     ids.close();
 
     // NOTE: this block of code uses 'OPEN_PULSE' mode in order to read example data
-    status = ids.open("imas:mdsplus?path=./testdb_mdsplus",OPEN_PULSE);
+    status = ids.open("imas:mdsplus?path=./testdb_mdsplus", OPEN_PULSE);
     // Access Layer API shares 3 different methods of interpolating values from DBEntry
     // PREVIOUS_SAMPLE
     // CLOSEST_SAMPLE¶
     // INTERPOLATION
 
-    // this part of code presents PREVIOUS_INTERP. It is interpolation method that returns the previous time slice if the requested time does not exactly exist in the original IDS
+    // this part of code presents PREVIOUS_SAMPLE. It is interpolation method that returns the previous time slice if the requested time does not exactly exist in the original IDS
     // if requested time is outside of time array, first, or last slice will be returned respectively
     ids._summary.getSlice(1.75, PREVIOUS_SAMPLE);
     summary = ids._summary;
 
     // previous time value for 1.75 is 1.0
     // summary/global_quantities/ip/value and time=1 is 10.0
-    std::cout<< "summary/global_quantities/ip/value for time=1" << summary.global_quantities.ip.value << "(Should be 10.0)"<<"\n";
+    std::cout<< "summary/global_quantities/ip/value for time=1: " << summary.global_quantities.ip.value << " (Should be 10.0)"<<"\n";
 
-    // this part of code presents CLOSEST_INTERP. It is interpolation method that returns the closest time slice in the original IDS
+    // this part of code presents CLOSEST_SAMPLE. It is interpolation method that returns the closest time slice in the original IDS
     // if requested time is equally spaced between two time slices, slice with higher index will be returned
     ids._summary.getSlice(1.75, CLOSEST_SAMPLE);
     summary = ids._summary;
     
     // closest time value to 1.75 is 2.0
     // value for summary/global_quantities/ip/value and time=2 is 11.0
-    std::cout<< "summary/global_quantities/ip/value for time=1" << summary.global_quantities.ip.value << "(Should be 10.0)"<<"\n";
+    std::cout<< "summary/global_quantities/ip/value for time=1: " << summary.global_quantities.ip.value << " (Should be 10.0)"<<"\n";
 
     // this part of code presents INTERPOLATION. It is interpolation method that returns a linear interpolation between the existing slices before and after the requested time.
     // NOTE: The linear interpolation will be successful only if, between the two time slices of an interpolated dynamic array of structure,
@@ -116,7 +115,7 @@ void read_slice()
     summary = ids._summary;
     
     // interpolated value for summary/global_quantities/ip/value and time=1.75 is 10.75
-    std::cout<< "summary/global_quantities/ip/value for time=1" << summary.global_quantities.ip.value << "(Should be 10.75)"<<"\n";
+    std::cout<< "summary/global_quantities/ip/value for time=1: " << summary.global_quantities.ip.value << " (Should be 10.75)"<<"\n";
 
 }
 
@@ -125,6 +124,3 @@ void partial_get()
     // The C++ interface does not support partial_get
     ;
 }
-
-
-    
