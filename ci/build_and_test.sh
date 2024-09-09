8338eff929ed6fa9eb75566d551bf421370396a1
Ludovic Fleury <ludovic.fleury@cea.fr>
2024-09-09 08:40:51 +0200

    Removing AL_CORE_VERSION and AL_PLUGINS_VERSION attributes

--- ci/build_and_test.sh
+++ ci/build_and_test.sh
@@ -122,9 +122,7 @@ CMAKE_ARGS=(${CMAKE_ARGS[@]}
     -D AL_DOWNLOAD_DEPENDENCIES=ON
     -D AL_COMMON_GIT_REPOSITORY=https://git.iter.org/scm/imas/al-common.git
     -D AL_CORE_GIT_REPOSITORY=https://git.iter.org/scm/imas/al-core.git
-    -D AL_CORE_VERSION=develop
     -D AL_PLUGINS_GIT_REPOSITORY=https://git.iter.org/scm/imas/al-plugins.git
-    -D AL_PLUGINS_VERSION=develop
     -D DD_GIT_REPOSITORY=https://git.iter.org/scm/imas/data-dictionary.git
     # DD version: can be set with DD_VERSION env variable, otherwise use latest master/3
     -D DD_VERSION=${DD_VERSION:-master/3}
