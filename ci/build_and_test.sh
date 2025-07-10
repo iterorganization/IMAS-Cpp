4f93bad77915c640b756f9fea493962473bbbb71
sawantp1 <prasadparshuram.sawantdesai@iter.org>
2025-07-10 15:11:52 +0200

    updated structure to check parent of  is allocated

--- ci/build_and_test.sh
+++ ci/build_and_test.sh
@@ -95,7 +95,7 @@ module load "${MODULES[@]}"
 # Debuggging:
 echo "Done loading modules:"
 module list
-set -x
+
 
 # Create a local git configuration with our access token
 if [ "x$bamboo_HTTP_AUTH_BEARER_PASSWORD" != "x" ]; then
@@ -111,28 +111,32 @@ rm -rf build
 
 # CMake configuration:
 CMAKE_ARGS=(${CMAKE_ARGS[@]}
-    -D "CMAKE_INSTALL_PREFIX=$(pwd)/test-install/"
-    # Enable all backends
-    -D AL_BACKEND_HDF5=ON
-    -D AL_BACKEND_MDSPLUS=ON
-    -D AL_BACKEND_UDA=ON
-    # Build MDSplus models
-    -D AL_BUILD_MDSPLUS_MODELS=ON
-    # Download dependencies from HTTPS (using an access token):
-    -D AL_DOWNLOAD_DEPENDENCIES=ON
-    -D AL_CORE_GIT_REPOSITORY=https://git.iter.org/scm/imas/al-core.git
-    -D AL_PLUGINS_GIT_REPOSITORY=https://git.iter.org/scm/imas/al-plugins.git
-    -D DD_GIT_REPOSITORY=https://git.iter.org/scm/imas/data-dictionary.git
-    # DD version: can be set with DD_VERSION env variable, otherwise use latest main
-    -D DD_VERSION=${DD_VERSION:-main}
-    # HLI options
-    -D AL_EXAMPLES=ON
-    -D AL_TESTS=ON
-    -D AL_PLUGINS=ON
-    # Build documentation
-    -D AL_HLI_DOCS=ON
-    # Work around Boost linker issues on 2020b toolchain
-    -D Boost_NO_BOOST_CMAKE=ON
+  -D "CMAKE_INSTALL_PREFIX=$(pwd)/test-install/"
+  # Enable all backends
+  -D AL_BACKEND_HDF5=${AL_BACKEND_HDF5:-ON}
+  -D AL_BACKEND_MDSPLUS=${AL_BACKEND_MDSPLUS:-ON}
+  -D AL_BACKEND_UDA=${AL_BACKEND_UDA:-ON}
+  # Build MDSplus models
+  -D AL_BUILD_MDSPLUS_MODELS=${AL_BUILD_MDSPLUS_MODELS:-ON}
+  # Download dependencies from HTTPS (using an access token):
+  -D AL_DOWNLOAD_DEPENDENCIES=${AL_DOWNLOAD_DEPENDENCIES:-ON}
+  -D AL_CORE_GIT_REPOSITORY=${AL_CORE_GIT_REPOSITORY:-https://git.iter.org/scm/imas/al-core.git}
+  -D AL_PLUGINS_GIT_REPOSITORY=${AL_PLUGINS_GIT_REPOSITORY:-https://git.iter.org/scm/imas/al-plugins.git}
+  # AL_PLUGINS version: can be set with AL_PLUGINS_VERSION env variable, otherwise use latest main
+  -D AL_PLUGINS_VERSION=${AL_PLUGINS_VERSION:-main}
+  -D DD_GIT_REPOSITORY=${DD_GIT_REPOSITORY:-https://github.com/iterorganization/IMAS-Data-Dictionary.git}
+  # DD version: can be set with DD_VERSION env variable, otherwise use latest main
+  -D DD_VERSION=${DD_VERSION:-main}
+  # AL Core version: can be set with AL_CORE_VERSION env variable, otherwise use latest main
+  -D AL_CORE_VERSION=${AL_CORE_VERSION:-main}
+  # HLI options
+  -D AL_EXAMPLES=${AL_EXAMPLES:-ON}
+  -D AL_TESTS=${AL_TESTS:-ON}
+  -D AL_PLUGINS=${AL_PLUGINS:-ON}
+  # Build documentation
+  -D AL_HLI_DOCS=${AL_HLI_DOCS:-ON}
+  # Work around Boost linker issues on 2020b toolchain
+  -D Boost_NO_BOOST_CMAKE=${Boost_NO_BOOST_CMAKE:-ON}
 )
 # Note: compilers are set as environment variables in the Bamboo config
 cmake -B build "${CMAKE_ARGS[@]}"
